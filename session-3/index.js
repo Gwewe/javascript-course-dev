62df12b6637add464d0a39360360bbfc821601ae
Wedna Guirand <wedna.guirand@gmail.com>
2023-10-31 19:34:59 +0000

    added a function

--- session-3/index.js
+++ session-3/index.js
@@ -1,13 +1,26 @@
-// This is a comment in JavaScript
-// You can see where this file is connected to your HTML in the index.html on line 14
-let a = 5
-const b = 10
-var c = a + b
-console.log("Original ouput for c:" + c)
+// // This is a comment in JavaScript
+// // You can see where this file is connected to your HTML in the index.html on line 14
+// let a = 5
+// const b = 10
+// var c = a + b
+// console.log("Original ouput for c:" + c)
 
-a = 20
+// a = 20
 
-console.log("And no change for c:" + c)
+// console.log("And no change for c:" + c)
 
-c = a + b
-console.log("There we go:" + c)
+// c = a + b
+// console.log("There we go:" + c)
+
+function sayHey() {
+    console.log("Bonjour!");
+}
+
+
+function conversation() {
+    sayHey();
+    console.log("How are you?");
+    console.log("Bonne soirée!");
+}
+
+conversation()
