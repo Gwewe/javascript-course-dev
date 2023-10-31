ea870c5ee7305cadceb3034601e5bd6e9af1d74d
Wedna Guirand <wedna.guirand@gmail.com>
2023-10-31 19:16:09 +0000

    completed first task

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,13 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let a = 5
+const b = 10
+var c = a + b
+console.log("Original ouput for c:" + c)
+
+a = 20
+
+console.log("And no change for c:" + c)
+
+c = a + b
+console.log("There we go:" + c)
